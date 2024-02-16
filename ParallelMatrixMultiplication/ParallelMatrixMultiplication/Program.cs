f594cbd8bdd2d6538349e7488e4f6ce5d740332d
GitRadium <gidradiumcode@gmail.com>
2024-02-16 16:34:10 +0300

    First thread test

--- ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
+++ ParallelMatrixMultiplication/ParallelMatrixMultiplication/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var otherThread = new Thread(() =>
+{
+    while (true)
+    {
+        Console.WriteLine("Hello from other thread!");
+    }
+});
+
+otherThread.Start();
+
+while (true)
+{
+    Console.WriteLine("Hello from this thread!");
+}
